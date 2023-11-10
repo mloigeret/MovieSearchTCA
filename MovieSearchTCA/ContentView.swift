ee1687f025926741f093bbfef65d26304118491e
Manuel Loigeret <manu@smarthalo.bike>
2023-11-10 16:24:07 -0500

    Initial Commit

---
+++ MovieSearchTCA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieSearchTCA
+//
+//  Created by Manuel Loigeret on 2023-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieSearchTCA/MovieSearchTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchTCAApp.swift
+//  MovieSearchTCA
+//
+//  Created by Manuel Loigeret on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
